605a66872129fe161903491bca4c8afd0a78a97f
Draldo <draldo_lopez@hotmail.com>
2016-08-11 00:53:24 -0400

    Dialog working with toast and snackbar

--- app/src/main/java/com/example/retrofitsimple/MainActivity.java
+++ app/src/main/java/com/example/retrofitsimple/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.retrofitsimple;
 
 import android.os.StrictMode;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -47,7 +49,8 @@ public class MainActivity extends AppCompatActivity implements MyDialogFragment.
             Toast.makeText(this, "Login successfully", Toast.LENGTH_LONG).show();
         }
         else{
-            Toast.makeText(this, "Wrong password for " + dialog.getArguments().getString("name"), Toast.LENGTH_LONG).show();
+            View view = findViewById(R.id.main_view);
+            Snackbar.make(view, "Wrong password for " + dialog.getArguments().getString("name"), Snackbar.LENGTH_SHORT).show();
         }
     }
 }
